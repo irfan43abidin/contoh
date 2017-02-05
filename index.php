611f254a3c83264a280afcddcbff19a3815790b3
irfan43abidin <irfan43abidin@gmail.com>
2017-02-05 23:50:08 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<title>halaman awal operasi crud</title>
+<style>
+div.container {
+	width: 100%;
+	border: 1px solid gray;
+}
+
+header, footer{
+	padding: 1cm;
+	color: white;
+	background-color: brown;
+	clear: left;
+	text-align: center;
+}
+
+nav {
+	float: left;
+	max-width: 160px;
+	margin: 0;
+	padding: 1cm;
+}
+
+nav ul {
+	list-style-type: none:
+	padding: 0;
+}
+nav ul a {
+	text-decoration: none;
+}
+
+article {
+	margin-left:170px;
+	border-left: 1px solid gray;
+	padding: 1cm;
+	overflow: hidden;
+}
+
+</style>
+
+</head>
+<body>
+
+<div class="container">
+
+<header>
+	<h1>operasi crud</h1>
+</header>
+
+<nav>
+	<ul>
+	<li><b>menu</b></li>
+	<li><a href="#">Menu 1</a></li>
+	<li><a href="#">Menu 2</a></li>
+	<li><a href="#">Menu 3</a></li>
+	</ul>
+</nav>
+
+<aticle>
+<table border="1" width="75%" align="center">
+
+<tr>
+	<th><a href="tambah.php"><button
+	type="submit">+tambah</button></a>
+	</td>
+</tr>
+<tr>
+	<td>No</td>
+	<td>nama></td>
+	<td>alamat</td>
+	<td>aksi</td>
+</tr>
+</table>
+</article>
+<footer>nama-nim</footer>
+</div>
+</body>
+</html>
+	
---
+++ tambah.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<title>halaman awal operasi crud</title>
+<style>
+div.container {
+	width: 100%;
+	border: 1px solid gray;
+}
+
+header, footer{
+	padding: 1cm;
+	color: white;
+	background-color: brown;
+	clear: left;
+	text-align: center;
+}
+
+nav {
+	float: left;
+	max-width: 160px;
+	margin: 0;
+	padding: 1cm;
+}
+
+nav ul {
+	list-style-type: none:
+	padding: 0;
+}
+nav ul a {
+	text-decoration: none;
+}
+
+article {
+	margin-left:170px;
+	border-left: 1px solid gray;
+	padding: 1cm;
+	overflow: hidden;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<div class="container">
+
+<header>
+<h1> Operasi CRUD </h1>
+</header>
+
+<nav>
+<ul>
+<li><a href="#">Menu 1</a></li>
+<li><a href="#">Menu 2</a></li>
+<li><a href="#">Menu 3</a></li>
+</ul>
+</nav>
+
+<article>
+<form>
+<table border="1" width="60%" align="center">
+<tr>
+<th align="center" colspan="2>Tambah Data</th>
+</tr>
+<tr>
+<td>Nama</td>
+<td><input type="text" name="nama" size="80"></td>
+</tr>
+<tr>
+<td>NIM</td>
+<td><input type="text" name="nim" size="80"></td>
+</tr>
+<tr>
+<td>Alamat</td>
+<td><input type="text" name="alamat" size="80"></td>
+</tr>
+<tr>
+<td colspan="2" align="right"><input type="button" value="Tambah"> <input type="button" value+"Batal"></td>
+</tr>
+</table>
+</form>
+</article>
+
+<Footer>NAMA-NIM</footer>
+</div>
+</body>
+</html>
